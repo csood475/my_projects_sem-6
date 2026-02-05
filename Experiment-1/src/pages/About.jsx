a29e7db6cf0d2d5a31ee6888b4519a245a29c2db
charu sood <23bai70044@cuchd.in>
2026-02-05 10:16:55 +0530

    unncessary files deleted

--- Experiment-1/src/pages/About.jsx
+++
@@ -1,9 +0,0 @@
-const About = () => {
-  return (
-    <div className="page-content">
-      <h2>This is the About Page</h2>
-    </div>
-  );
-};
-
-export default About;
--- Experiment-1/src/pages/Home.jsx
+++
@@ -1,7 +0,0 @@
-const Home = () => (
-  <div className="page-content">
-    <h2>This is the Home Page</h2>
-    <p>This is a Single Page Application built manually using React State.</p>
-  </div>
-);
-export default Home;
